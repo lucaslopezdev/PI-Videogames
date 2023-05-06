57160c86b748c92b04fd4a67278059d2ba657315
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-05-06 20:18:45 -0300

    Update Models

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    platforms: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    releaseDate: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    }
+  }, { timestamps: false});
 };
